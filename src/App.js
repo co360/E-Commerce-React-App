db3e1325b5fa41e0c3949f5093ed12d22a76df61
carlpadilla <carlpadila@protonmail.com>
2019-08-16 15:12:30 -0400

    building sign in component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/ShopPage.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/Sign-in-and-sign-up.component';
 import Header from './components/header/Header.component';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop/' component={ShopPage} /> {/* Testing */}
+        <Route exact path='/signin' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
