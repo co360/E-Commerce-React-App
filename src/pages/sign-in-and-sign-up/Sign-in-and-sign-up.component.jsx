2b12a6f185e84ef76444520afb8303d32befb2f0
carlpadilla <carlpadila@protonmail.com>
2019-08-30 12:05:13 -0400

    implemented firebase utils, included ability for users to sign up with email and password

--- src/pages/sign-in-and-sign-up/Sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/Sign-in-and-sign-up.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/SignIn.component';
+import SignUp from '../../components/Sign-up/Signup.component';
 
 import './sign-in-and-sign-up.styles.scss';
 
@@ -7,6 +8,7 @@ const SignInAndSignUp = () => {
   return (
     <div className='sign-in-and-sign-up'>
       <SignIn />
+      <SignUp />
     </div>
   );
 };
