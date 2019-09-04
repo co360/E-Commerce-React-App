8eb9fa99834eb9e9c72ed8ef275d191debaff6ba
carlpadilla <carlpadila@protonmail.com>
2019-09-04 15:14:57 -0400

    added cart icon and dropdown along wiht caer reducer

--- src/components/header/Header.component.jsx
+++ src/components/header/Header.component.jsx
@@ -9,7 +9,7 @@ import CartDropdown from '../Cart-dropdown/Cart-dropdown.component';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link to='/' className='logo-container'>
       <Logo className='logo' />
@@ -33,12 +33,13 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon />
     </div>
-    <CartDropdown />
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
