f9861f5da77a402a254adbf518e46bdc05c87298
carlpadilla <carlpadila@protonmail.com>
2019-09-05 12:02:58 -0400

    styled items page with add to cart buttons, implemented cart reducer.

--- src/components/custom-button/CustomBtn.component.jsx
+++ src/components/custom-button/CustomBtn.component.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 import './custombtn.styles.scss';
 
-const CustomBtn = ({ children, isGoogleSignIn, ...otherProps }) => (
+const CustomBtn = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
   <button
-    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+    className={`${inverted ? 'inverted' : ''} ${
+      isGoogleSignIn ? 'google-sign-in' : ''
+    } custom-button`}
     {...otherProps}
   >
     {children}
