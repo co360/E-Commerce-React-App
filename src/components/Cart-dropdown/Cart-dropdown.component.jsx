dc30455898e009b1cdd2a905fecb1ed28f47ea1b
Carl Padilla <carlpadilla@protonmail.com>
2020-03-22 15:23:12 -0400

    checkout dropdown hides after click

--- src/components/Cart-dropdown/Cart-dropdown.component.jsx
+++ src/components/Cart-dropdown/Cart-dropdown.component.jsx
@@ -7,8 +7,9 @@ import { withRouter } from 'react-router-dom';
 import CustomBtn from '../custom-button/CustomBtn.component';
 import CartItem from '../cart-item/Cart-item';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {cartItems.length ? (
@@ -19,7 +20,12 @@ const CartDropdown = ({ cartItems, history }) => (
         <span className='empty-message'>Your cart is empty</span>
       )}
     </div>
-    <CustomBtn onClick={() => history.push('/checkout')}>
+    <CustomBtn
+      onClick={() => {
+        history.push('/checkout');
+        dispatch(toggleCartHidden());
+      }}
+    >
       GO TO CHECKOUT
     </CustomBtn>
   </div>
